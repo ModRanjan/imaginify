c637fad33abba2603657bcb4b1c4be2c4284725a
mod ranjan <modranjan1999@gmail.com>
2024-02-19 03:42:17 +0530

    added types

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+// ====== URL QUERY PARAMS
+declare type FormUrlQueryParams = {
+  searchParams: string;
+  key: string;
+  value: string | number | null;
+};
+
+declare type UrlQueryParams = {
+  params: string;
+  key: string;
+  value: string | null;
+};
+
+declare type RemoveUrlQueryParams = {
+  searchParams: string;
+  keysToRemove: string[];
+};
