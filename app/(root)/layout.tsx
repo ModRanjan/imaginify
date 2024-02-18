2340b538b2f29338837471f23951b5ab414faccc
mod ranjan <modranjan1999@gmail.com>
2024-02-19 04:05:40 +0530

    set-up sign-in / sign-up functionality (with clerk)

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import { Toaster } from "@/components/ui/toaster";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      {/* <Sidebar /> */}
+      {/* <MobileNav /> */}
+
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+
+      <Toaster />
+    </main>
+  );
+};
+
+export default Layout;
